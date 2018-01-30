c327cd150d6a67ca72a68e5106750bf0c3ced528
maddiereddy <maddiereddy@gmail.com>
2018-01-29 19:18:49 -0800

    minor corrections naming conventions

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 'use strict'
 
-const numResults = 15;
-const API_Key = "AIzaSyDgaHyAAK2rxOI-YNmP7ilXcQm2LdTDmwE";
+const NUM_RESULTS = 15;
+const API_KEY = "AIzaSyDgaHyAAK2rxOI-YNmP7ilXcQm2LdTDmwE";
 const YOUTUBE_URL = "https://www.googleapis.com/youtube/v3/search";
 
 function getDataFromAPI(searchTerm, callback) {
   const settings = {
       part: 'snippet',
-      key: `${API_Key}`,
+      key: `${API_KEY}`,
       q: `${searchTerm}`,
       type: 'video',
-      maxResults: numResults
+      maxResults: NUM_RESULTS
     };
 
   $.getJSON(YOUTUBE_URL, settings, callback);
@@ -33,19 +33,18 @@ function renderResult(result) {
 }
 
 function displayData(data) {
-  $('.js-results-header').html(`Top ${numResults} results`).prop('hidden', false);
+  $('#results-header').html(`Top ${NUM_RESULTS} results`).prop('hidden', false);
   const results = data.items.map((item, index) => renderResult(item));
-  $('.js-search-results').html(results).prop('hidden', false);
+  $('#search-results').html(results).prop('hidden', false);
 }
 
 
 function watchSubmit() {
-  $('.js-search-form').submit(event => {
-    event.preventDefault();
-
-    const queryTarget = $(event.currentTarget).find('.js-query');
+  $('#search-form').submit(event => {
+    const queryTarget = $(event.currentTarget).find('#query');
     const query = queryTarget.val();
-
+    
+    event.preventDefault();
     getDataFromAPI(query, displayData);
   })
 }
